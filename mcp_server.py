63ce6ccca361616ede5180e5fb5ff4e806bc6291
venkat <venkat.py3@gmail.com>
2026-03-03 17:22:11 +0530

    first commit

---
+++ mcp_server.py
@@ -0,0 +1,48 @@
+from dataclasses import Field
+
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP("DocumentMCP", log_level="ERROR")
+
+
+docs = {
+    "deposition.md": "This deposition covers the testimony of Angela Smith, P.E.",
+    "report.pdf": "The report details the state of a 20m condenser tower.",
+    "financials.docx": "These financials outline the project's budget and expenditures.",
+    "outlook.pdf": "This document presents the projected future performance of the system.",
+    "plan.md": "The plan outlines the steps for the project's implementation.",
+    "spec.txt": "These specifications define the technical requirements for the equipment.",
+}
+
+# tool to read a doc
+@mcp.tool(
+    name="read_doc",
+    description="Read the contents of a document and return it as a string.",
+    args={"doc_id": "The ID of the document to read."},
+)
+def read_doc(doc_id: str):
+    if doc_id not in docs:
+        raise ValueError(f"Document with ID '{doc_id}' not found.")
+    return docs[doc_id]
+
+# tool to edit a doc
+@mcp.tool(
+    name="edit_document",
+    description="Edit document by replacing a string in the document with a new string.",
+)
+def edit_document(
+    doc_id: str = Field(description="The ID of the document that will be edited."),
+    old_str: str = Field(description="The text to replace. Must exactly, including whitespace."),
+    new_str: str = Field(description="The new text to insert in place of the old text.")):
+    if doc_id not in docs:
+        raise ValueError(f"Document with ID '{doc_id}' not found.")
+    docs[doc_id] = docs[doc_id].replace(old_str, new_str)
+
+# TODO: Write a resource to return all doc id's
+# TODO: Write a resource to return the contents of a particular doc
+# TODO: Write a prompt to rewrite a doc in markdown format
+# TODO: Write a prompt to summarize a doc
+
+
+if __name__ == "__main__":
+    mcp.run(transport="stdio")
