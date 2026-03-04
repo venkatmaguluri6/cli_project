a05e11f6fa564026841c24794a6be1857a7d3803
venkat <venkat.py3@gmail.com>
2026-03-04 12:16:15 +0530

    Added code to define resource(list_doc, fetch_doc)  in mcp_server

--- mcp_server.py
+++ mcp_server.py
@@ -1,6 +1,6 @@
 from pydantic import Field
 from mcp.server.fastmcp import FastMCP
-
+from mcp.server.fastmcp.prompts import base
 mcp = FastMCP("DocumentMCP", log_level="ERROR")
 
 
@@ -36,9 +36,48 @@ def edit_document(
         raise ValueError(f"Document with ID '{doc_id}' not found.")
     docs[doc_id] = docs[doc_id].replace(old_str, new_str)
 
-# TODO: Write a resource to return all doc id's
-# TODO: Write a resource to return the contents of a particular doc
-# TODO: Write a prompt to rewrite a doc in markdown format
+# resource to return all doc id's
+@mcp.resource(
+    "docs://documents",
+    mime_type="application/json"
+)
+def list_docs() -> list[str]:
+    return list(docs.keys())
+
+# resource to return the contents of a particular doc
+@mcp.resource(
+    "docs://documents/{doc_id}",
+    mime_type="text/plain"
+)
+def fetch_doc(doc_id: str) -> str:
+    if doc_id not in docs:
+        raise ValueError(f"Document with ID '{doc_id}' not found.")
+    return docs[doc_id]
+
+# prompt to rewrite a doc in markdown format
+@mcp.prompt(
+    name="format",
+    description="Rewrite the contents of the docuement in markdown format."
+)
+def format_document(
+    doc_id: str = Field(description="Id of the document to format.")
+) -> list[base.Message]:
+    prompt = f"""
+                Your goal is to reformat a document to be written with markdown syntax.
+
+                The id of the document you need to reformat is:
+                <document_id>
+                {doc_id}
+                </document_id>
+
+                Add in headers, bullet points, tables, etc as necessary. Feel free to add in structure.
+                Use the 'edit_document' tool to edit the document. After the document has been reformatted...
+                """
+    
+    return [
+        base.UserMessage(prompt)
+    ]
+
 # TODO: Write a prompt to summarize a doc
 
 
